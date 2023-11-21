adec50f79650a25f059310151fac8caad218296b
max-fluff <pusm@omegafuture.ru>
2023-11-21 19:51:26 +0300

    progress

---
+++ Assets/_App/Source/ViewPresenterPairs/Narrative/DraggableObjectView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Lean.Transition;
+using UnityEngine;
+
+namespace MaxFluff.Prototypes.Narrative
+{
+    public abstract class DraggableObjectView : ViewBase
+    {
+        public TapProcessor TapProcessor;
+        public LeanPlayer PuttingBackTransition;
+        public LeanPlayer GettingUpTransition;
+    }
+
+    public abstract class DraggableObjectPresenter : PresenterBase<DraggableObjectView>
+    {
+        public event Action OnStartDrag;
+
+        protected DraggableObjectPresenter(DraggableObjectView view) : base(view)
+        {
+            view.TapProcessor.OnTappped.AddListener(OnTapped);
+        }
+
+        private void OnTapped()
+        {
+            OnStartDrag?.Invoke();
+            _view.TapProcessor.enabled = false;
+        }
+
+        public void StartDrag()
+        {
+            _view.GettingUpTransition.Begin();
+        }
+
+        public void SetPosition(Vector2 position)
+        {
+            var transformPosition = _view.transform.position;
+            transformPosition.x = position.x;
+            transformPosition.z = position.y;
+            _view.transform.position = transformPosition;
+        }
+
+        public void Reset()
+        {
+            _view.PuttingBackTransition.Begin();
+            _view.TapProcessor.enabled = true;
+        }
+    }
+}
